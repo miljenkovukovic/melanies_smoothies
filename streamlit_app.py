81457211f7a1e053db001b83c89754717683eb6b
miljenkovukovic <62656889+miljenkovukovic@users.noreply.github.com>
2024-03-10 12:08:50 +0100

    Update streamlit_app.py
    Fruityvice integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
@@ -40,3 +38,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)
